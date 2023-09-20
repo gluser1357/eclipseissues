f5e6db7f960ed027d190aa2d593cb1509c81837a
sre <s.reifegerste@mapchart.com>
2023-09-20 15:18:23 +0200

    remove unused project folder

--- project/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-open module gluser1357.project {
-	requires gluser1357.util;
-	requires gluser1357.core;
-}
--- project/src/test/java/gluser1357/project/TesterOverloadedTest.java
+++
@@ -1,6 +0,0 @@
-package gluser1357.project;
-
-import gluser1357.tester.Tester;
-
-public class TesterOverloadedTest
-extends Tester {}
